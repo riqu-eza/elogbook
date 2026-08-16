91fa027286ee65671c83d66ad053e803bd8625ac
kang`ethe <offline@kali.dkmuthunga.com>
2026-08-16 11:25:50 +0300

    built

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
-// src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
+
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Optional: for debugging
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
